2876acaad3dbfde6f2daa026357be0d599df2367
AyselPro <AyselLuckyPro@gmail.comgit config --global user.email AyselLuckyPro@gmail.com>
2024-02-13 14:13:42 +0300

    change README

---
+++ LocalizeProject/DetailViewControllers/StoryboardOnlyLocalization/StoryboardOnlyLocalizationViewController.swift
@@ -0,0 +1,12 @@
+//
+//  StoryboardOnlyLocalizationViewController.swift
+//  LocalizeProject
+//  Created by Aysel on 11.02.2024.
+//
+
+import UIKit
+
+class StoryboardOnlyLocalizationViewController: UIViewController {
+    
+    @IBOutlet weak var textLabel: UILabel!
+}
